12928b4b35cf2fd81b9dcee4824d79de53e7ab67
mauricedummel <maurice.duemmel@gmail.com>
2022-11-02 13:59:47 +0100

    views.py hinzugefügt, enthält das routing mittels Flaks von verschiedenen URLS an die entsprechenden HTML templates

---
+++ Frontend/views.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, request
+from .models import Website
+from flask_login import login_required, current_user
+from . import db
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/")
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+
+@views.route("/scrape", methods=['GET', 'POST'])
+@login_required
+def scrape():
+    if request.method == "POST":
+        scraping_regex = request.form.get("regex")
+        scraping_url = request.form.get("url")
+
+        new_scraping_request = Website(url=scraping_url, regex=scraping_regex)
+        db.session.add(new_scraping_request)
+        db.session.commit()
+    else:
+        return render_template("scraping.html", user=current_user)
