5e44cf672b51d9674f4c72f17714caeda53d64e9
acx810 <maurice.duemmel@haw-hamburg.de>
2022-11-09 15:01:19 +0100

    get_id vom login_manager überschrieben, damit Prüfung auf Tabellen korrekt funktioniert

--- Frontend/models.py
+++ Frontend/models.py
@@ -8,14 +8,17 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    last_name = db.Column(db.String(150))
+    # last_name = db.Column(db.String(150))
     account_creation_date = db.Column(db.DateTime(timezone=True), default=func.now())
 
     website = db.relationship("Website", backref="user", lazy=True)
 
+    def get_id(self):
+        return (self.user_id)
+
 
 class Website(db.Model):
-    website_id = db.Column(db.Integer, primary_key=True)
+    website_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=False)
 
     url = db.Column(db.String(1000))
@@ -23,11 +26,16 @@ class Website(db.Model):
     regex = db.Column(db.String(500))
     search_string = db.Column(db.String(500))
 
+    def get_id(self):
+        return (self.website_id)
 
 
 class Data(db.Model):
-    data_id = db.Column(db.Integer, primary_key=True)
+    data_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     website_id = db.Column(db.Integer, db.ForeignKey("website.website_id"), nullable=False)
 
-    content = db.Column(db.String(10000))
+    content = db.Column(db.String(2000))
     content_date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    def get_id(self):
+        return (self.data_id)
