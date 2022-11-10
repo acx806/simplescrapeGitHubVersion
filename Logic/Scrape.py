2b5f767470ec3855f73ba49b599b5e2581d9a322
mduem <m.duemel@gmx.de>
2022-11-10 23:40:18 +0100

    Grundlegende Scraping Funktion für Produktverfügbarkeit hinzugefügt

--- Logic/Scrape.py
+++ Logic/Scrape.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
-class Scrape():
+class Scrape:
+    """
+    Usage example:
+    product1_scrape = Scrape(
+        "https://beemybox.de/products/fahrradbox-basic-mit-zahlenschloss-solar-powerbank",
+        "Ausverkauft")
+
+    product1_scrape.get_html()
+    product1_scrape.is_available()
+    """
 
     def __init__(self, product_url, search_string):
         self.product_url = product_url
@@ -13,18 +23,16 @@ class Scrape():
         self.soup = BeautifulSoup(self.html, 'html.parser')
 
     def is_available(self):
-
+        regex = r"<(\S*?)[ >]"
         matched_tags = self.soup.find_all(lambda tag: len(tag.find_all()) == 0 and self.search_string in tag.text)
 
+        accepted_tags = ["button", "p", "h1", "h2"]
+
         for tag in matched_tags:
-            print(tag.unwrap())
-            #temp_soup = BeautifulSoup(tag,'html.parser')
-            #print("HEY NEW TAG!!!"+temp_soup.prettify()+"\n\n\n\n\n\n\n")
+            regex_matches = re.match(regex, str(tag))
 
+            if regex_matches.group(1) in accepted_tags:
+                print(self.search_string + " found in \"" + regex_matches.group(1) + "\" tag")
+                return False
 
-product1_scrape = Scrape(
-    "https://beemybox.de/products/fahrradbox-basic-mit-zahlenschloss-solar-powerbank",
-    "Ausverkauft")
-
-product1_scrape.get_html()
-product1_scrape.is_available()
+        return True
