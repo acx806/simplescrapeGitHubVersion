4f2dc7a310874db856194dede4399b93bb2c440e
mduem <m.duemel@gmx.de>
2022-11-13 15:39:11 +0100

    Einbindung des Scraping in Datenbankobjekte und Frontend in views.py Datenbank Model Anpassung

--- Frontend/auth.py
+++ Frontend/auth.py
@@ -47,18 +47,20 @@ def sign_up():
 
         user = User.query.filter_by(email=email).first()
         if user:
+            print("EXISTS ALRDY")
             flash('Email already exists.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
 
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
             print("pw dont match")
+        elif len(password1) < 7:
+            print("pw not long enough")
             flash('Password must be at least 7 characters.', category='error')
         else:
             new_user = User(email=email, password=generate_password_hash(
-                password1, method='sha256'),user_id=uuid4().int)
+                password1, method='sha256'), user_id=uuid4().int)
             db.session.add(new_user)
             db.session.commit()
             print("COMMITED TO DB")
