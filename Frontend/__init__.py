49fcd0b30b8a9ddd96ec68717eb364c049a99e9e
mduem <m.duemel@gmx.de>
2022-11-29 14:09:36 +0100

    Reformat für deployment

--- Frontend/__init__.py
+++ Frontend/__init__.py
@@ -27,6 +27,7 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
+    print(app.root_path)
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
